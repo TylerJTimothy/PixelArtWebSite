a498f9e71c1b93023a194b178b5a0c9e753a77e2
Tyler Timothy <tylerjtimothy@gmail.com>
2023-12-08 22:59:23 -0700

    login service

---
+++ pixelArt/database.js
@@ -0,0 +1,68 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('pixelArt');
+const userCollection = db.collection('user');
+const scoreCollection = db.collection('score');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+  async function saveImage(imageData) {
+    try {
+        const result = await imageCollection.insertOne({ imageData });
+        console.log(`Image saved with id: ${result.insertedId}`);
+    } catch (ex) {
+        console.error(`Error saving image: ${ex.message}`);
+        throw ex;
+    }
+}
+
+async function getImages() {
+    try {
+        const images = await imageCollection.find({}).toArray();
+        return images;
+    } catch (ex) {
+        console.error(`Error retrieving images: ${ex.message}`);
+        throw ex;
+    }
+}
+
+module.exports = { getUser,
+    getUserByToken,
+    createUser,
+    saveImage, 
+    getImages };
+
---
+++ pixelArt/index.js
@@ -0,0 +1,143 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
+
+// The service port may be set on the command line
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the applications static content
+app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+app.get('/generatePalette', async (req, res) => {
+    const userInput = req.query.text; // Get user input from query parameter
+
+    const url = `https://onesimpleapi.com/api/color?token=Gba0UUY8v2moTwRK1MZHXmYbi0EA2Z0t0zrBbZZQ&output=json&hue=125&text=${encodeURIComponent(userInput)}`;
+
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred');
+    }
+});
+
+app.post('/saveImage', async (req, res) => {
+    try {
+        await db.saveImage(req.body.imageData); // Save the image data to MongoDB
+        res.send('Canvas saved successfully!');
+    } catch (error) {
+        console.error('Error saving image:', error);
+        res.status(500).send('Error saving the image');
+    }
+});
+
+app.get('/getImages', async (req, res) => {
+    try {
+        const images = await db.getImages(); // Retrieve the images from MongoDB
+        res.json(images); // Send them back as JSON
+    } catch (error) {
+        console.error('Error retrieving images:', error);
+        res.status(500).send('Error retrieving images');
+    }
+});
